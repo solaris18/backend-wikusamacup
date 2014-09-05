e83067bda4df7fe38e6226b827fae386bbba19cb
Agung Hari Wijaya <a9un9_ch@yahoo.co.id>
2014-09-05 15:39:17 +0700

    send email on registration

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
+use Mailgun\Mailgun;
 $app = new Slim\Slim();
 
 // routes
@@ -19,9 +20,41 @@ function registration()
         $response->header('Access-Control-Allow-Origin', '*');
 
     try {
-        $data = $app->request->post();
-        $registration = Registration::create($data);
-            $response->write( json_encode( [ 'id' => $registration->id, 'error' => false ] ) );
+      $data = $app->request->post();
+      $registration = Registration::create($data);
+
+      # Include the Autoloader (see "Libraries" for install instructions)
+
+      # Instantiate the client.
+      $mgClient = new Mailgun('key-93ff6cb32c74d74e5ae33267adb7758b');
+      $domain = "sandbox0a8d2b3620114db389abe6833a0c5c96.mailgun.org";
+
+      # Make the call to the client.
+      $result = $mgClient->sendMessage("$domain",
+        array('from'    => 'Wikusama Cup <postmaster@wikusamacup.com>',
+              'to'      => 'Dwi Ma\'ruf Alvansuri <dwimarufalvansuri@gmail.com>',
+              'bcc'      => 'Agung Hari Wijaya <a9un9_ch@yahoo.co.id>',
+              'subject' => '[Wikusama Cup] New Team Register',
+              'html'    => '
+                <html>
+                  <body>
+                    <p>Hi , Dwi Ma\'ruf Alvansuri</p>
+                    <p>just notification '.$app->request->post('team_name').' team success register with this detail : </p>
+                    <p>
+                      <strong>Team Name </strong> : '.$app->request->post('team_name').'<br>
+                      <strong>Generation </strong> : '.$app->request->post('generation').'<br>
+                      <strong>PIC Name </strong> : '.$app->request->post('pic').'<br>
+                      <strong>Phone Number </strong> : '.$app->request->post('phone').'<br>
+                      <strong>Email </strong> : '.$app->request->post('email').'<br>
+                      <strong>Sosial Media </strong> : '.$app->request->post('sosmed').'<br>
+                    </p>
+                  </body>
+                </html>
+              ')
+        );
+    
+
+      $response->write( json_encode( [ 'id' => $registration->id, 'error' => false ] ) );
     } catch (Exception $e) {
             $response->write( json_encode( [ 'error' => true ] ) );
     }
